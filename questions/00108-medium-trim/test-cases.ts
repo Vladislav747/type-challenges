a8643cd7aea35a04326da0827a3743123d8ffe4f
vlad <zippo585@gmail.com>
2023-09-20 00:01:10 +0500

    made medium-deep-readonly

--- questions/00108-medium-trim/test-cases.ts
+++ questions/00108-medium-trim/test-cases.ts
@@ -1,5 +1,12 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+/**
+ * Chars what need to be remove
+ */
+type Chars = ' ' | '\n' | '\t'
+
+type Trim<S extends string> = S extends `${Chars}${infer SS}` ? Trim<SS> : S extends `${infer SS}${Chars}` ? Trim<SS> : S
+
 type cases = [
   Expect<Equal<Trim<'str'>, 'str'>>,
   Expect<Equal<Trim<' str'>, 'str'>>,
